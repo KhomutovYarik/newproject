9b0d672b0ef2689c027b34001db47e18d5bc1713
dima059 <userdima059@gmail.com>
2020-06-09 15:49:26 +0500

    Revert "Changes on button"
    This reverts commit b59b77f3c9122cb54c62a7c7a96fe6dfe1498638.

--- newproject/Form1.Designer.cs
+++ newproject/Form1.Designer.cs
@@ -33,22 +33,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(456, 130);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Location = new System.Drawing.Point(316, 126);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(243, 123);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Кнопка!";
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
